6439b7cd75a9900497d18efa61a489c45f4a3c61
Mustafa <mustafa31112@gmail.com>
2022-02-13 22:23:17 +0000

    update files to parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -22,8 +22,8 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-ORDER BY teacher`)
+WHERE cohorts.name = $1
+ORDER BY teacher`, [`${process.argv[2]}`])
 .then(res => {
   console.log(res.rows)
   res.rows.forEach(user => {
